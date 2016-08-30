b6670e1f2a2065ad9b599c1a042e56cc56653f47
Josh Adams <josh.rubyist@gmail.com>
2016-08-30 16:02:36 -0500

    Add tests to make sure world is empty/not when expeted

--- src/World.elm
+++ src/World.elm
@@ -1,17 +1,59 @@
-module World exposing (init, livingCellsCount)
+module World exposing (init, isEmpty, setLivingAt, isAliveAt, tick)
+
+import Array exposing (Array)
+
+
+type alias Position =
+    ( Int, Int )
 
 
 type alias Model =
-    { cells : List Int
-    }
+    Array (Array Bool)
 
 
 init : Model
 init =
-    { cells = []
-    }
+    initialCells 10
 
 
-livingCellsCount : Model -> Int
-livingCellsCount model =
-    0
+tick : Model -> Model
+tick model =
+    model
+
+
+arrayIsAllFalse : Array Bool -> Bool
+arrayIsAllFalse array =
+    array
+        |> Array.foldl (\a b -> a || b) False
+        |> not
+
+
+isEmpty : Model -> Bool
+isEmpty model =
+    model
+        |> Array.map (not << arrayIsAllFalse)
+        |> arrayIsAllFalse
+
+
+setLivingAt : Position -> Model -> Model
+setLivingAt ( x, y ) model =
+    let
+        existingRow =
+            Array.get x model
+                |> Maybe.withDefault (Array.repeat (Array.length model) False)
+    in
+        model
+            |> Array.set x
+                (existingRow
+                    |> Array.set y True
+                )
+
+
+isAliveAt : Position -> Model -> Bool
+isAliveAt position model =
+    True
+
+
+initialCells : Int -> Array (Array Bool)
+initialCells size =
+    Array.repeat size (Array.repeat size False)
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -10,6 +10,30 @@ all : Test
 all =
     describe "Conway's Game of Life"
         [ test "a new world is empty" <|
-            always <|
-                Expect.equal 0 (World.livingCellsCount World.init)
+            (\() ->
+                World.init
+                    |> World.isEmpty
+                    |> Expect.true "Expected the world to be empty"
+            )
+        , test "a cell can be added to the world" <|
+            (\() ->
+                World.init
+                    |> World.setLivingAt ( 1, 1 )
+                    |> World.isAliveAt ( 1, 1 )
+                    |> Expect.true "Expected the world to have an alive cell at (1, 1)"
+            )
+        , test "after adding a cell, the world is not empty" <|
+            (\() ->
+                World.init
+                    |> World.setLivingAt ( 1, 1 )
+                    |> World.isEmpty
+                    |> Expect.false "Expected the world to not be empty"
+            )
+        , test "an empty world stays empty after a tick" <|
+            (\() ->
+                World.init
+                    |> World.tick
+                    |> World.isEmpty
+                    |> Expect.true "Expected the world to be empty"
+            )
         ]
